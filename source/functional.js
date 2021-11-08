eb0f787b9af161f0fdc05441bcc5997cbddcd837
Andrei Nemes <simply.andrei@gmail.com>
2021-11-08 22:06:01 +0000

    add arity and additional functions

--- source/functional.js
+++ source/functional.js
@@ -9,10 +9,27 @@ export function curryN(len, carry, fn) {
   }
 }
 
-// todo: test this
 export const curry = fn => curryN(fn.length, [], fn);
 
-export const prop = curry((key, object) => object[key]);
+export const arity = curry((n, fn) => {
+  switch (n) {
+    case 0: return function() { return fn(); };
+    case 1: return function(a0) { return fn(a0); };
+    case 2: return function(a0, a1) { return fn(a0, a1); };
+    case 3: return function(a0, a1, a2) { return fn(a0, a1, a2); };
+    case 4: return function(a0, a1, a2, a3) { return fn(a0, a1, a2, a3); };
+    case 5: return function(a0, a1, a2, a3, a4) { return fn(a0, a1, a2, a3, a4); };
+    case 6: return function(a0, a1, a2, a3, a4, a5) { return fn(a0, a1, a2, a3, a4, a5); };
+    case 7: return function(a0, a1, a2, a3, a4, a5, a6) { return fn(a0, a1, a2, a3, a4, a5, a6); };
+    default: return fn;
+  }
+});
 
-export const VERSION = '0.1.1';
+export const unary = arity(1);
+export const binary = arity(2);
+
+export const prop = curry((key, object) => object[key]);
+export const has = curry((key, object) => object.hasOwnProperty(key));
+
+export const VERSION = '0.2.0';
 
--- test/functional.spec.js
+++ test/functional.spec.js
@@ -1,4 +1,4 @@
-import { curry, prop, VERSION } from '../source/functional.js';
+import { curry, prop, has, arity, unary, binary, VERSION } from '../source/functional.js';
 
 suite(`functional #${VERSION}`, function() {
   suite('curry', function() {
@@ -15,18 +15,89 @@ suite(`functional #${VERSION}`, function() {
 
   suite('prop', function() {
     test('returns property value', function() {
-      const object = { tokyo: 'san francisco' };
-      const result = prop('tokyo', object);
+      const object = { city: 'tokyo' };
+      const result = prop('city', object);
 
-      expect(result).to.equal('san francisco');
+      expect(result).to.equal('tokyo');
     });
 
     test('can be partially applied', function() {
-      const object = { tokyo: 'san francisco' };
-      const partial = prop('tokyo');
+      const object = { city: 'tokyo' };
+      const partial = prop('city');
       const result = partial(object);
 
-      expect(result).to.equal('san francisco');
+      expect(result).to.equal('tokyo');
+    });
+  });
+
+  suite('has', function() {
+    test('is true if property exists', function() {
+      const object = { city: 'tokyo' };
+      const result = has('city', object);
+
+      expect(result).to.equal(true);
+    });
+
+    test('is false if property does not exist', function() {
+      const object = { city: 'tokyo' };
+      const result = has('country', object);
+
+      expect(result).to.equal(false);
+    });
+
+    test('can be partially applied', function() {
+      const object = { city: 'tokyo' };
+      const partial = has('city');
+      const result = partial(object);
+
+      expect(result).to.equal(true);
+    });
+  });
+
+  suite('arity', function() {
+    test('can remove arguments', function() {
+      const four = (a, b, c, d) => [a, b, c, d];
+      const result = arity(0, four);
+
+      expect(result.length).to.equal(0);
+      expect(result(1, 2, 3, 4)).to.deep.equal(new Array(4).fill(undefined));
+    });
+
+    test('can limit number of arguments', function() {
+      const four = (a, b, c, d) => [a, b, c, d];
+      const result = arity(2, four);
+
+      expect(result.length).to.equal(2);
+      expect(result(1, 2, 3, 4)).to.deep.equal([1, 2, undefined, undefined]);
+    });
+
+    test('can be partially applied', function() {
+      const four = (a, b, c, d) => [a, b, c, d];
+      const unary = arity(1);
+      const result = unary(four);
+
+      expect(result.length).to.equal(1);
+      expect(result(1, 2, 3, 4)).to.deep.equal([1, undefined, undefined, undefined]);
+    });
+  });
+
+  suite('unary', function() {
+    test('limits arguments to one', function() {
+      const four = (a, b, c, d) => [a, b, c, d];
+      const result = unary(four);
+
+      expect(result.length).to.equal(1);
+      expect(result(1, 2, 3, 4)).to.deep.equal([1, undefined, undefined, undefined]);
+    });
+  });
+
+  suite('binary', function() {
+    test('limits arguments to two', function() {
+      const four = (a, b, c, d) => [a, b, c, d];
+      const result = binary(four);
+
+      expect(result.length).to.equal(2);
+      expect(result(1, 2, 3, 4)).to.deep.equal([1, 2, undefined, undefined]);
     });
   });
 });
